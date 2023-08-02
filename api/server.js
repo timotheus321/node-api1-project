60b07efdb1d19b9a9c4a26bab723a0d54817bde4
Timothy White <bigtball321@yahoo.com>
2023-08-01 20:01:03 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,12 +1,25 @@
-// BUILD YOUR SERVER 
 const express = require('express')
-
+const User = require('./users/model')
 const server = express()
 
-server.use('*', (req, res) =>{
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
+
+server.use('*', (req, res) => {
     res.status(404).json({
         message: 'not found'
     })
 })
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+module.exports = server; 
